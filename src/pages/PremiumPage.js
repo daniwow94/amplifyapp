17436a01f08d5ecf57bbc0968d427fe5c498b661
daniel-pintar <93781228+daniel-pintar@users.noreply.github.com>
2022-05-18 14:22:32 +0200

    TextFieldTest

--- src/pages/PremiumPage.js
+++ src/pages/PremiumPage.js
@@ -1,5 +1,19 @@
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, TextField } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { TextField } from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    field: {
+        marginTop: 20,
+        marginBottom: 20,
+        display: 'block'
+    }
+})
+
+function Create() {
+    const classes = useStyles()
+
 
 const PremiumPage = () => {
     return (
@@ -8,11 +22,21 @@ const PremiumPage = () => {
                 <div>
                     <h1>Hello, welcome to my Website</h1>
                     <h3>You are authenticated!! This is the premium content page!!</h3>
+                    <form noValidate autoComplete='off'>
+                        <TextField 
+                            className={classes.field}
+                            label="AWS Access Key Id"
+                            variant="outlined"
+                            fullWidth
+                            required
+                        />
+                    </form>
                     <button onClick={signOut}>Sign Out</button>
                 </div>
             )}
         </Authenticator>
     );
+};
 };
 
 export default PremiumPage;
