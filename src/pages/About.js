f88bfa9b33b613b86eadce89e07f05725eeb0e37
daniel-pintar <93781228+daniel-pintar@users.noreply.github.com>
2022-05-18 13:22:50 +0200

    loggin

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,22 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 const About = () => {
-    return <h1>This is the About Page</h1>
+    return (
+        <Authenticator>
+            {({ signOut }) =>(
+                <div>
+                    <h1>This is the About Page</h1>
+                    <button onClick={signOut}>Sign Out</button>
+                </div>
+            )}
+        </Authenticator>
+    );
 };
+
+
+/*const About = () => {
+    return <h1>This is the About Page</h1>
+};*/
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,22 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 const Home = () => {
-    return <h1>Home page v2</h1>
+    return (
+        <Authenticator>
+            {({ signOut }) =>(
+                <div>
+                    <h1>Home page v2</h1>
+                    <button onClick={signOut}>Sign Out</button>
+                </div>
+            )}
+        </Authenticator>
+    );
 };
+
+
+/*const Home = () => {
+    return <h1>Home page v2</h1>
+};*/
 
 export default Home;
